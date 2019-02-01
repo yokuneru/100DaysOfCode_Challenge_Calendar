b327164bcfec15199352bf6322e7766a5f4d89e8
yokuneru <yokuneru.gs@gmail.com>
2019-02-01 13:14:13 +0900

    クッキーで継続日数を取得できるよう調整中。 未完成です。

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
 $(function(){
   'use strict';
+
   var comment = "";
-  var days = 1;
+  var days = 0;
 
   $("#form").on("change keyup", function(){
     comment = $(this).val();
 
     $("#twBtn").attr("href", "https://twitter.com/intent/tweet?text=" + comment + "&hashtags=" + "100DaysOfCode," + "Day_" + days);
-
   });
 
 
 //動作確認用
   $(document).mousedown(function(){
-    console.log( $("#twBtn").attr("href") );
-    console.log(comment);
+    // console.log( $("#twBtn").attr("href") );
   });
+
+
+  $(document).mousedown(function(){
+    $.cookie("visit", "hey", {expires: 7});
+    var visCnt = $.cookie("visit");
+    console.log(visCnt);
+  });
+
+
+
+
 
 });

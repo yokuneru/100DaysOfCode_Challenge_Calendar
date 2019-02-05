6629e7b43be296495ffca4ac72ae712d357e8dbf
yokuneru <yokuneru.gs@gmail.com>
2019-02-05 15:10:54 +0900

    日付のカウント設定を修正してローンチできる状態まで完成！

--- js/main.js
+++ js/main.js
@@ -3,29 +3,34 @@ $(function(){
 
   //変数の定義・データ取得
   var today;
+  var windowH = $(window).height();
+  var comment;
 
   //ローカルストレージ記録の取得
   var dayCnt = localStorage.getItem("dayCnt");
   var startDate = localStorage.getItem("startDate");
   var lastAccess = localStorage.getItem("lastAccess");
-  showLog();
+  // console.log("access log");
+  // showLog();
 
-  //ウィンドウサイズの反映
-  var windowH = $(window).height();
-  if(windowH < 766) windowH = 766;
-  $(".base").css("height", windowH * 3);
-  $("#view-frame, #intro-page, #void, #main-page").css("height", windowH);
 
-  //日付の取得
-  (function getDate(){
+  function onload(){
+    //ウィンドウサイズの反映
+    if(windowH < 766) windowH = 766;
+    $(".base").css("height", windowH * 4);
+    $("#view-frame, #intro-page, #void, #main-page").css("height", windowH);
+
+    //日付の取得
     var dt = new Date();
     var year = dt.getFullYear();
     var month = ("0" + (dt.getMonth() + 1)).slice(-2);
-    var date = ("0" + dt.getDate()).slice(-2);
-    var hour = ("0" + dt.getHours()).slice(-2);
-    var min = ("0" + dt.getMinutes()).slice(-2);
-    today = "" + year + "/" + month + "/" + date + "/" + hour + "/" + min;
-  }());
+    var date = ("0" + dt.getDate()).slice(-2) ;
+    // var hour = ("0" + dt.getHours()).slice(-2);
+    // var min = ("0" + dt.getMinutes()).slice(-2);
+    today = "" + year + "/" + month + "/" + date;
+  }
+  onload();
+
 
 
   //チャレンジしていない場合
@@ -35,17 +40,21 @@ $(function(){
 
     //今日から始める
     $("#new-challenge").click(function(){
+      //main-pageに移動
       var target = $("#main-page").offset().top;
       $("#view-frame").animate({scrollTop:target}, 400, "swing");
 
       dayCnt = 1;
       $("#dayCnt").text(dayCnt);
-      $(".comment").text("Day" + dayCnt + " #100DaysOfCode")
+      $(".comment").text("Day" + dayCnt + " " + "\n#100DaysOfCode");
+      comment = document.fm.tarea.value;
       $("#start-date").text(today);
       $(".lastAccess").css("visibility", "hidden");
       localStorage.setItem("dayCnt", dayCnt);
       localStorage.setItem("startDate", today);
       localStorage.setItem("lastAccess", today);
+      // console.log("challenge day1");
+      // showLog();
     });
 
 
@@ -64,24 +73,27 @@ $(function(){
         $("#continue-start").css("opacity", 1);
         $("#continue-days").css("font-size", "25px");
       }else{
-        $("#continue-start").css("opacity", 0.5);
         $("#continue-start").removeClass("accept");
+        $("#continue-start").css("opacity", 0.5);
         $("#continue-days").css("font-size", "12px");
       }
     });
 
     $('#continue-start').click(function(){
       if(dayCnt > 0 && Math.round(dayCnt) == dayCnt){
+        //main-pageに移動
         var target = $("#main-page").offset().top;
         $("#view-frame").animate({scrollTop:target}, 400, "swing");
 
         $("#dayCnt").text(dayCnt);
-        $(".comment").text("Day" + dayCnt + " #100DaysOfCode")
+        $(".comment").text("Day" + dayCnt + " " + "\n#100DaysOfCode");
+        comment = document.fm.tarea.value;
         $(".startDate").css("visibility", "hidden");
         $(".lastAccess").css("visibility", "hidden");
         localStorage.setItem("dayCnt", dayCnt);
         localStorage.setItem("lastAccess", today);
-        showLog();
+        // console.log("challenge continue");
+        // showLog();
       }
     });
 
@@ -96,10 +108,12 @@ $(function(){
       dayCnt = dayCnt - 0 + 1;
       localStorage.setItem("dayCnt", dayCnt)
       localStorage.setItem("lastAccess", today);
-      showLog();
+      // console.log("add dayCnt");
+      // showLog();
     }
     $("#dayCnt").text(dayCnt);
-    $(".comment").text("Day" + dayCnt + " #100DaysOfCode")
+    $(".comment").text("Day" + dayCnt + " " + "\n#100DaysOfCode");
+    comment = document.fm.tarea.value;
 
     //開始日の表示設定
     if(startDate == undefined){
@@ -114,7 +128,7 @@ $(function(){
   $(".comment").on("click change keyup", function(){
     $(this).addClass("comment-anm");
 
-    var comment = document.fm.tarea.value;
+    comment = document.fm.tarea.value;
     $("#textCnt").text(280 - comment.length);
     if(comment.length < 270){
       $("#textCnt").css("color", "rgb(150, 150, 150)")
@@ -123,23 +137,31 @@ $(function(){
     }else {
       $("#textCnt").css("color", "rgb(200, 60, 60)")
     }
-
-    $("#tw-btn a").attr("href", "https://twitter.com/intent/tweet?hashtags=100DaysOfCode&text=" + comment);
   });
 
+
+  //tweet
+  $("#tw-btn a").click(function(){
+    $("#tw-btn a").attr("href", "https://twitter.com/intent/tweet?hashtags=100DaysOfCode&text=" + comment);
+  });
 
 
   //学習日数の修正
   $("#modify").click(function(){
     var modifyCnt = window.prompt("学習した日数を入力してください","") - 0;
     if(modifyCnt > 0 && Math.round(modifyCnt) === modifyCnt){
+      comment = document.fm.tarea.value;
+      comment = comment.replace(dayCnt, modifyCnt);
+      document.fm.tarea.value = comment;
+
       dayCnt = modifyCnt;
       $("#dayCnt").text(dayCnt);
       localStorage.setItem("dayCnt", dayCnt);
-      showLog();
+      // console.log("modify dayCnt");
+      // showLog();
     }else if(modifyCnt == 0){
     }else {
-      alert("学習した日数は整数で入力してください")
+      alert("学習した日数は1日以上で入力してください")
     }
   });
 
@@ -158,26 +180,30 @@ $(function(){
 
 
   //開発用検証コマンド
-  $(document).on("keypress", function(e){
-    if(e.keyCode == 101){
-      //Rキー 記録削除
-      localStorage.removeItem("dayCnt");
-      localStorage.removeItem("startDate");
-      localStorage.removeItem("lastAccess");
-      console.log("data-remove");
-      showLog();
-    }else if(e.keyCode == 115){
-      //Sキー　ログを見る
-      console.log("show-log");
-      showLog();
-    }
-  });
-
-  function showLog(){
-    console.log("dayCnt = " + localStorage.getItem("dayCnt"));
-    console.log("startDate = " + localStorage.getItem("startDate"));
-    console.log("lastAccess = " + localStorage.getItem("lastAccess"));
-    console.log("-----------------------");
-  }
+  // $(document).on("keypress", function(e){
+  //   if(e.keyCode == 101){
+  //     //Rキー 記録削除
+  //     localStorage.removeItem("dayCnt");
+  //     localStorage.removeItem("startDate");
+  //     localStorage.removeItem("lastAccess");
+  //     console.log("data-remove");
+  //     showLog();
+  //   }else if(e.keyCode == 115){
+  //     //Sキー　ログを見る
+  //     console.log("show-log");
+  //     showLog();
+  //   }else if(e.keyCode == 102){
+  //     //Fキー
+  //   }
+  //   console.log(e.keyCode);
+  // });
+  //
+  //
+  // function showLog(){
+  //   console.log("dayCnt = " + localStorage.getItem("dayCnt"));
+  //   console.log("startDate = " + localStorage.getItem("startDate"));
+  //   console.log("lastAccess = " + localStorage.getItem("lastAccess"));
+  //   console.log("-----------------------");
+  // }
 
 });
